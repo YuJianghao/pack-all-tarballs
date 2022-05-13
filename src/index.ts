d8ecc4148587d2e3f87b570cf4fe7f8077e5422a
winwin2011 <yjh2011@live.com>
2022-05-14 06:24:03 +0800

    feat: custom target and path

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-import path from 'path'
-import fs from 'fs/promises'
-import { getAllDepsFromPackageLockJson } from './parser/package-lock'
-import { packList } from './lib'
+import {resolve} from "path"
+import fs from "fs/promises"
+import { Command } from "commander"
+import { getAllDepsFromPackageLockJson } from "./parser/package-lock"
+import { packList } from "./lib"
 
-const FOLDER = 'tarballs';
-(async () => {
-  const PACKAGE_LOCK_JSON = path.resolve(process.cwd(), 'package-lock.json')
-  const jsonString = await fs.readFile(PACKAGE_LOCK_JSON, 'utf8').catch((err) => {
-    console.error('file to read package-lock.json')
-    console.error(err)
-    process.exit(1)
-  })
-  const deps = getAllDepsFromPackageLockJson(jsonString)
-  if (!await fs.access(FOLDER).then(() => true, () => false))
-    await fs.mkdir(FOLDER)
-  await packList(deps, { cwd: path.resolve(process.cwd(), FOLDER) })
-})()
-
+const program = new Command()
+program
+  .description("des")
+  .argument("<target>", "folder to save tarballs, default to `tarballs`")
+  .option("-p, --path <path>", "path to package-lock.json")
+  .action(async (target = "tarballs", path = "package-lock.json") => {
+    const PACKAGE_LOCK_JSON = resolve(path)
+    const jsonString = await fs
+      .readFile(PACKAGE_LOCK_JSON, "utf8")
+      .catch((err) => {
+        console.error("file to read package-lock.json")
+        console.error(err)
+        process.exit(1)
+      })
+    const deps = getAllDepsFromPackageLockJson(jsonString)
+    if (
+      !(await fs.access(target).then(
+        () => true,
+        () => false
+      ))
+    )
+      await fs.mkdir(target)
+    await packList(deps, { cwd: path.resolve(process.cwd(), target) })
+  }).parse()
