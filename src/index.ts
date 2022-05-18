510a2288ff5d5bed1c29c36ac31c783a5832b3ec
winwin2011 <yjh2011@live.com>
2022-05-19 06:29:11 +0800

    chore: show version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { Command } from 'commander'
+import { version as currentVersion } from '../package.json'
 import { packList } from './lib'
 import { getAllDepsFromLockFile } from './parser'
 
@@ -9,14 +10,22 @@ program
   .argument('[file]', 'lock file path')
   .option('-o, --output <output>', 'folder to save tarballs', 'tarballs')
   .option('-t, --threads <threads>', 'threads count to run parallel', '32')
+  .option('-v, --version', 'show current version')
   .action(
     async (file: string, {
       threads,
       output,
+      version,
     }: {
       output: string
       threads: string
+      version: boolean
     }) => {
+      if (version) {
+        // eslint-disable-next-line no-console
+        console.log(`pack-all-tarballs v${currentVersion}`)
+        return
+      }
       const deps = await getAllDepsFromLockFile(file)
       if (!fs.existsSync(output))
         fs.mkdirSync(output)
